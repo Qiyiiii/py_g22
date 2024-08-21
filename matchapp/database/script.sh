8ddccc0334757208773b50ce23ddaf17ad58bbe0
Qiyi Z <qiyi.zhang@mail.utoronto.ca>
2024-08-21 03:28:53 -0400

    1

--- matchapp/database/script.sh
+++ matchapp/database/script.sh
@@ -5,3 +5,4 @@ sqlite3 matchapp/database/matchapp.db < matchapp/database/InsertUserUS.sql
 sqlite3 matchapp/database/matchapp.db < matchapp/database/InsertInterests.sql
 sqlite3 matchapp/database/matchapp.db <matchapp/database/InsertActions.sql
 sqlite3 matchapp/database/matchapp.db <matchapp/database/InsertLocations.sql
+python3 matchapp/app/app.py
