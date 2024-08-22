ce904ab54ce4bc20527362880bbea2584915bdcd
Qiyi Z <qiyi.zhang@mail.utoronto.ca>
2024-08-22 11:31:07 -0400

    bug fix

--- matchapp/app/static/js/index.js
+++ matchapp/app/static/js/index.js
@@ -6,6 +6,9 @@ function loginUser() {
     let userId = prompt("Please enter your User ID:");
     if (userId && Number.isInteger(Number(userId))) {
         window.location.href = `/profile/${userId}`;}
+    else if(!userId){
+
+    }
 
     else {
         alert("Please enter a valid user id");
